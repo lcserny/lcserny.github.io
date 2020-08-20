10f2d64429cf45c0e04b5fbd25fb8af8220ec5a2
Leonardo Cserny <leo.cserny@gmail.com>
2020-08-20 13:33:07 +0300

    refactor and improve UI

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
 });
 
 function bindToggleMainHover(main) {
+    if (main == null) {
+        console.log("Element with ID 'main' not found");
+        return;
+    }
+
     let mainContent = document.getElementById("main-content");
+    if (mainContent == null) {
+        console.log("Element with ID 'main-content' not found");
+        return;
+    }
+
     main.addEventListener("mouseenter", function (event) {
        mainContent.classList.add("hover");
     });
